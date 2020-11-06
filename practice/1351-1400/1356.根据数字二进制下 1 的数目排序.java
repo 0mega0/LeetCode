ce0607d156a79b2242958b8f548bf8862c85a827
0mega0 <1592387105@qq.com>
2020-11-06 18:42:02 +0800

    Create 1356.根据数字二进制下 1 的数目排序.java
    大佬nb

---
+++ practice/1351-1400/1356.根据数字二进制下 1 的数目排序.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] sortByBits(int[] arr) {
+        int[] map = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            map[i] = Integer.bitCount(arr[i]) * 10000000 + arr[i];
+        }
+        Arrays.sort(map);
+        for (int i = 0; i < map.length; i++) {
+            map[i] = map[i] % 10000000;
+        }
+        return map;
+    }
+}
+//不愧是大佬，一个数组存两个数据
